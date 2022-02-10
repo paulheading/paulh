8f19ce85f84783e697ac38e5e0dbc71bbb56ebf8
Paul Heading <paulheading@me.com>
2022-02-10 07:06:34 -0600

    better ts definitions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import 'styles/globals/index.scss'
-import type { AppProps } from 'next/app'
+import type { AppProps, NextWebVitalsMetric } from 'next/app'
 import { Layout } from 'components'
 import 'focus-visible/dist/focus-visible.min.js'
 import { Provider } from 'react-redux'
 import { store } from 'state'
 import { env } from 'scripts/variables'
 
-export function reportWebVitals(metric:any) {
+export function reportWebVitals(metric: NextWebVitalsMetric) {
   if (env.is('local')) {
     if (metric.label === 'web-vital') {
       console.log(metric)
