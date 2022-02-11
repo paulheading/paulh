ec14e2e9229c6c4caa92b529e9e000f271757dca
Paul Heading <paulheading@me.com>
2022-02-11 07:30:26 -0600

    add moment note

--- components/resume/card/status.tsx
+++ components/resume/card/status.tsx
@@ -8,6 +8,7 @@ interface Status {
 }
 
 export const Status: React.FC<Status> = ({ dueComplete, due, start, loading }) => {
+  // https://dockyard.com/blog/2020/02/14/you-probably-don-t-need-moment-js-anymore
   const date = (target: string) => new Date(target).toLocaleDateString('en-gb',{ year: 'numeric', month: 'short' });
   return loading ? <Loading lines={[0.6]} /> : <span>{ dueComplete ? `Finished: ${date(due)}` : `Started: ${date(start)}` }</span>
 }
